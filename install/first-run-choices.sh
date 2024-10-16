7e80d92ac5998ff7e2eb1b7a1240867e4159f75f
Gabriel <gncorreia@protonmail.com>
2024-10-16 19:36:15 -0300

    feat: installing bitwarden if chosen in gum

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,5 +1,5 @@
 OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox" "Bitwarden")
-DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
+DEFAULT_OPTIONAL_APPS='Spotify,Zoom,Bitwarden'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
